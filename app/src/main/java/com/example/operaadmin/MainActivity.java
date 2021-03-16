e09b4d13814e18cc9e038bfd9af35bbc5b148e0d
Saket Kumar <saketcool35@gmail.com>
2021-03-16 16:32:45 +0530

    5th commit

--- app/src/main/java/com/example/operaadmin/MainActivity.java
+++ app/src/main/java/com/example/operaadmin/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.github.ybq.android.spinkit.SpinKitView;
@@ -14,11 +16,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton fab;
     FirebaseUser firebaseUser;
     FirebaseAuth mAuth;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,28 @@ public class MainActivity extends AppCompatActivity {
         fab = findViewById(R.id.fab);
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        spinner = findViewById(R.id.spinner);
+
+        final List<String> song_category = new ArrayList<>();
+        song_category.add("HipHop");
+        song_category.add("Rock");
+        song_category.add("Sad");
+        song_category.add("Punjabi");
+        song_category.add("Back In 90's");
+        song_category.add("English");
+        song_category.add("English");
+        song_category.add("English");
+        song_category.add("English");
+        song_category.add("English");
+        song_category.add("English");
+        song_category.add("English");
+        song_category.add("English");
+        ArrayAdapter<String> dataAdapter;
+        dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,song_category);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setHasTransientState(true);
+        spinner.setAdapter(dataAdapter);
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
